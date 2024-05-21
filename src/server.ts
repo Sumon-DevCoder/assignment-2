9b73195a7285da3166d32a7e686d70d88c151ab5
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-05-21 20:38:14 +0600

    mongodb connection setup completed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import app from "./app";
-const port = 5000;
+import config from "./app/config";
+import mongoose from "mongoose";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    console.log("mongodb connect");
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
